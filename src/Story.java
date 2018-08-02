ca23b1b886b5b4f2b211b0371f29dd12a38afcc9
Arif <istiaque313@gmail.com>
2018-08-02 10:24:49 -0400

    How I feel about fairy tales

--- src/Story.java
+++ src/Story.java
@@ -4,6 +4,7 @@ public class Story {
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("Their tribe was named babodock and their name was known throughout the land.");
+		System.out.println("This fairy tale stuff are so wack!!");
 	}
 
 }
