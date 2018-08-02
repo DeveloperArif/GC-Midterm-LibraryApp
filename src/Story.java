f8d39d06b3881bffebf47b0a3c5aeebd7104a71c
Sasi K <kbsasikarthik@gmail.com>
2018-08-02 10:27:05 -0400

    story develops...

--- src/Story.java
+++ src/Story.java
@@ -5,6 +5,7 @@ public class Story {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("Their tribe was named babodock and their name was known throughout the land.");
 		System.out.println("This fairy tale stuff are so wack!!");
+		System.out.println("Kai-Yun loved his monkeys a lot!");
 	}
 
 }
