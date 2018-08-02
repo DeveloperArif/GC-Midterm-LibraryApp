ebefe866be26a0a7bb38e9ed89ab3a6764903ab4
Arif <istiaque313@gmail.com>
2018-08-02 10:45:54 -0400

    changes made

--- src/Story.java
+++ src/Story.java
@@ -2,12 +2,12 @@
 public class Story {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of students.");
 		System.out.println("Their tribe was named babodock and their name was known throughout the land.");
 		System.out.println("This fairy tale stuff are so wack!!");
-		System.out.println("Kai-Yun loved his monkeys a lot!");
+		System.out.println("Kai-Yun loved his students a lot!");
 		System.out.println("And wherever they went he followed them. He had a magical sword that allowed him to soar above the tress while they sought out their enemies. ");
-		System.out.println("Justin Bieber had a monkey too! (Dont' ask me how I know that)");
+		System.out.println("Justin Bieber had a student too! (Dont' ask me how I know that)");
 	}
 
 }
