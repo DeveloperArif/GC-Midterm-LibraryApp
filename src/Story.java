e3f212a1e7c25f59cdbc22940e743995b372bd6e
arjenkins <arobinsonjenkins@gmail.com>
2018-08-02 10:16:25 -0400

    added to story

--- src/Story.java
+++ src/Story.java
@@ -3,6 +3,7 @@ public class Story {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Their tribe was named babodock and their name was known throughout the land.");
 	}
 
 }
